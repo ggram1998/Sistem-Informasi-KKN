7415b4bbd0d3b569bd585e1f1448f62c87284db9
vickokakuse21 <vickokakuse21@gmail.com>
2019-04-09 01:16:47 +0700

    update new CI
    add page mahasiswa

---
+++ kkn/application/controllers/Wali.php
@@ -0,0 +1,68 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Wali extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	function index()
+	{
+		$data['title'] = "KKN - Dashboard";
+		$this->load->view('dashboard',$data);
+	}
+	
+	function HalamanProfil()
+	{
+		$data['title'] = "KKN - Profil";
+		$this->load->view('profil',$data);
+	}
+	function wali(){
+		$data['title'] = "KKN - wali";
+		$this->load->view('wali',$data);
+	}
+	function kuesioner(){
+		$data['title'] = "KKN - Kuesioner";
+		$this->load->view('kuesioner',$data);
+	}
+	function aduan(){
+		$data['title'] = "KKN - aduan";
+		$this->load->view('aduan',$data);
+	}
+	function catatan(){
+		$data['title'] = "KKN - catatan";
+		$this->load->view('catatan',$data);
+	}
+	function vidio(){
+		$data['title'] = "KKN - vidio";
+		$this->load->view('vidio',$data);
+	}
+	function harin(){
+		$data['title'] = "KKN - harin";
+		$this->load->view('harin',$data);
+	}
+	function akhir(){
+		$data['title'] = "KKN - akhir";
+		$this->load->view('akhir',$data);
+	}
+	function rencana(){
+		$data['title'] = "KKN - rencana";
+		$this->load->view('rencana',$data);
+	}
+	function desa(){
+		$data['title'] = "KKN - akhir";
+		$this->load->view('akhir',$data);
+	}
+}
