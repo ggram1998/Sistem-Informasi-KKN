0d285239cf53b5da1ddeaa52d8eb61b6a92d10ce
faisalbawono <38713099+faisalbawono@users.noreply.github.com>
2019-04-22 20:03:47 +0700

    Add files via upload

---
+++ database/db kkn/dpl_kkn.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 22 Apr 2019 pada 15.02
+-- Versi server: 10.1.31-MariaDB
+-- Versi PHP: 7.2.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dpl_kkn`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `dpl`
+--
+
+CREATE TABLE `dpl` (
+  `nip` int(20) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `dpl`
+--
+ALTER TABLE `dpl`
+  ADD PRIMARY KEY (`nip`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
